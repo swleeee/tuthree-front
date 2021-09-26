41730c60bf6913625aca659d347f597afcc7786f
swleeee <42002888+swleeee@users.noreply.github.com>
2021-09-26 20:03:06 +0900

    기능 추가 : 메인페이지 배너 임시 제작

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,26 @@ class Footer extends Component {
     return (
       <Container>
         Footer
+        <licence>
+          <a href="https://www.freepik.com/photos/business">
+            Business photo created by jcomp - www.freepik.com
+          </a>
+          <a href="https://www.freepik.com/photos/calendar">
+            Calendar photo created by rawpixel.com - www.freepik.com
+          </a>
+
+          <a href="https://www.freepik.com/vectors/business">
+            Business vector created by stories - www.freepik.com
+          </a>
+
+          <a href="https://www.freepik.com/vectors/school">
+            School vector created by macrovector - www.freepik.com
+          </a>
+
+          {/* <a href="https://www.freepik.com/vectors/idea">
+            Idea vector created by stories - www.freepik.com
+          </a> */}
+        </licence>
         <font>이사만루체 Copyright ⓒ (주)공게임즈. All Rights Reserved.</font>
       </Container>
     );
--- src/components/OuterContainer.js
+++ src/components/OuterContainer.js
@@ -6,7 +6,7 @@ const OuterContainer = styled.div`
   //   height: 60px;
   width: 100%;
 
-  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
+  // box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
   display: inline-flex;
   justify-content: center;
 `;
