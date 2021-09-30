2facdfc13609c021ec7dd1eeae9a5897f5024bab
swleeee <42002888+swleeee@users.noreply.github.com>
2021-09-30 20:15:34 +0900

    기능 추가 : 관리자 로그인 페이지(PC, 태블릿, 모바일) UI

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,9 +1,11 @@
 import Auth from './Account/Auth';
 import Common from './Common/Common';
 import Notice from './Community/Notice';
+import AdminAuth from './Admin/Auth';
 
 export default {
   Auth,
   Common,
   Notice,
+  AdminAuth,
 };
