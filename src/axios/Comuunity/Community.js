3ca36e02441f62a66e70617c8cd0ea6249098f6e
swleeee <42002888+swleeee@users.noreply.github.com>
2021-10-05 04:55:42 +0900

    기능 추가 : 사용자 커뮤니티 axios 정리

--- src/axios/Comuunity/Community.js
+++ src/axios/Comuunity/Community.js
@@ -9,3 +9,41 @@ export function getCommunity(req) {
     headers: req.headers ? req.headers : null,
   });
 }
+
+export function getDetailCommunity(req) {
+  return axios({
+    method: 'GET',
+    url: `${ROOT_URL}/community/id/${req.id}`,
+    params: req.params ? req.params : null,
+    headers: req.headers ? req.headers : null,
+  });
+}
+
+export function setDetailCommunity(req) {
+  return axios({
+    method: 'POST',
+    url: `${ROOT_URL}/community/write`,
+    params: req.params ? req.params : null,
+    headers: req.headers ? req.headers : null,
+    data: req.data,
+  });
+}
+
+export function putDetailCommunity(req) {
+  return axios({
+    method: 'PUT',
+    url: `${ROOT_URL}/community/write`,
+    params: req.params ? req.params : null,
+    headers: req.headers ? req.headers : null,
+    data: req.data,
+  });
+}
+
+export function delCommunity(req) {
+  return axios({
+    method: 'DELETE',
+    url: `${ROOT_URL}/community/${req.id}`,
+    params: req.params ? req.params : null,
+    headers: req.headers ? req.headers : null,
+  });
+}
