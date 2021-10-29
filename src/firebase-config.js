082caad8ff7bd464fae2cd7d7ad696476a627318
swleeee <42002888+swleeee@users.noreply.github.com>
2021-10-29 18:18:15 +0900

    gitignore 해제

---
+++ src/firebase-config.js
@@ -0,0 +1,9 @@
+export let config = {
+  apiKey: 'AIzaSyDI2_kLWmvIaFihYdqfY0bc81liemBR7vc',
+  authDomain: 'project-tuthree.firebaseapp.com',
+  projectId: 'project-tuthree',
+  storageBucket: 'project-tuthree.appspot.com',
+  messagingSenderId: '154599911586',
+  appId: '1:154599911586:web:9d73a0ff84b0f3ff65e7fb',
+  measurementId: 'G-1B70CDPDM4',
+};
